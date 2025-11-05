50bdcce7783e1a1fcf931103da40a0296c9fb1a3
Gage Winsand <gwinsand1@student.cvtc.edu>
2025-11-05 09:51:59 -0600

    Added simple directory structure. Added sanitize.php (imported from another project). Added a basic index.html to use as a base.

---
+++ sanitize.php
@@ -0,0 +1,42 @@
+<?php
+
+function sanitizeString($type, $field) {
+  
+  if ($type == INPUT_POST) {
+    if (isset($_POST[$field])) {
+      return htmlspecialchars(strip_tags($_POST[$field]));
+    }
+  } else if ($type == INPUT_GET) {
+    if (isset($_GET[$field])) {
+      return htmlspecialchars(strip_tags($_GET[$field]));
+    }
+  } else {
+    if (isset($_SERVER[$field])) {
+      return htmlspecialchars(strip_tags($_SERVER[$field]));
+    }
+  }
+}
+
+function sanitizeFloat($type, $field) {
+  if ($type == INPUT_POST) {
+    if (isset($_POST[$field])) {
+      return filter_input(INPUT_POST, $field, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+    }
+  } else if ($type == INPUT_GET) {
+    if (isset($_GET[$field])) {
+      return filter_input(INPUT_GET, $field, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+    }
+  }
+}
+
+function sanitizeInt($type, $field) {
+  if ($type == INPUT_POST) {
+    if (isset($_POST[$field])) {
+      return filter_input(INPUT_POST, $field, FILTER_SANITIZE_NUMBER_INT);
+    }
+  } else if ($type == INPUT_GET) {
+    if (isset($_GET[$field])) {
+      return filter_input(INPUT_GET, $field, FILTER_SANITIZE_NUMBER_INT);
+    }
+  }
+}
