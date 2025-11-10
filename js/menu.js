df509bee88aee4ee0723ee3bd478b63403d2853c
Gage Winsand <gwinsand1@student.cvtc.edu>
2025-11-10 10:34:22 -0600

    merged menuDev into development branch. basic start menu added.

---
+++ js/menu.js
@@ -0,0 +1,35 @@
+// button initialization
+$startBtn = $("#startBtn");
+$loginBtn = $("#loginBtn");
+$configBtn = $("#configBtn");
+$maintBtn = $("#maintBtn");
+$backBtn = $("#backBtn");
+
+$startBtn.on("click", ()=>{
+  console.log("Start Pressed");
+  $(".mainMenu").hide();
+  $backBtn.show();
+});
+
+$loginBtn.on("click", ()=>{
+  console.log("Login Pressed");
+  $(".mainMenu").hide();
+  $backBtn.show();
+});
+
+$configBtn.on("click", ()=>{
+  console.log("Config Pressed");
+  $(".mainMenu").hide();
+  $backBtn.show();
+});
+
+$maintBtn.on("click", ()=>{
+  console.log("Maint Pressed");
+  $(".mainMenu").hide();
+  $backBtn.show();
+});
+
+$backBtn.on("click", ()=>{
+  $(".mainMenu").show();
+  $backBtn.hide();
+});
