f5f2e7395b2e2ca802f513df578c5079a227f3ab
Gage Winsand <gwinsand1@student.cvtc.edu>
2025-11-10 10:24:49 -0600

    revised file structure. might need another revision to implement an Ajax focused version eventually

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PuzzleMaster</title>
+  <?php 
+  $_SESSION["state"] = 0;
+
+?>
+
+</head>
+
+<body>
+    
+  <div class="wrapper">
+    <?php
+      include "screens/mainMenu.php";
+    ?>
+  </div>
+
+</body>
+</html>
---
+++ screens/mainMenu.php
@@ -0,0 +1,8 @@
+    <button id="startBtn" class="mainMenu">Start Run</button> 
+    <button id="loginBtn" class="mainMenu">Login</button>  
+    <button id="configBtn" class="mainMenu">Config</button>
+    <button id="maintBtn" class="mainMenu">Maintenance</button>
+    <button id="backBtn" hidden=true>Back</button>
+
+    
+    <script src="../js/menu.js"></script>
