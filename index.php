ab56b69f900826af551255ceca32611403aa445c
mgadke3 <mgadke3@student.cvtc.edu>
2025-11-11 14:19:08 -0600

    linked the css file

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PuzzleMaster</title>
+  <link rel="stylesheet" href="css/mainMenu.css">
   <?php 
   $_SESSION["state"] = 0;
 
