c4bce44a51b2b907023cf172ce9872194e9dd47b
mgadke3 <mgadke3@student.cvtc.edu>
2025-11-11 14:20:44 -0600

    Added labels to each visible button

--- screens/mainMenu.php
+++ screens/mainMenu.php
@@ -1,8 +1,13 @@
-    <button id="startBtn" class="mainMenu">Start Run</button> 
-    <button id="loginBtn" class="mainMenu">Login</button>  
-    <button id="configBtn" class="mainMenu">Config</button>
-    <button id="maintBtn" class="mainMenu">Maintenance</button>
-    <button id="backBtn" hidden=true>Back</button>
-
+    <div class="buttons">
+        <h3>Logged in?</h3>
+        <button id="startBtn" class="mainMenu">Start Run</button> 
+        <h3>Need to make an account?</h3>
+        <button id="loginBtn" class="mainMenu">Login</button>  
+        <h3>Change Settings?</h3>
+        <button id="configBtn" class="mainMenu">Config</button>
+        <h3>Dev tools:</h3>
+        <button id="mainBtn" class="mainMenu">Maintenance</button>
+        <button id="backBtn" hidden=true>Back</button>
+    </div>
     
     <script src="../js/menu.js"></script>
