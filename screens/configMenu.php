9359548547e96d72a6a770ffb51ca983431272f5
mgadke3 <mgadke3@student.cvtc.edu>
2025-11-18 22:36:05 -0600

    Config layout changes for styling

--- screens/configMenu.php
+++ screens/configMenu.php
@@ -1,7 +1,11 @@
     <div class="buttons configMenu">
-      <label for="set1">Setting 1</label>      
-      <input type="checkbox" name="set1" id="">
+      <div class="option">
+        <label for="set1">Setting 1</label>      
+        <input type="checkbox" name="set1" id="">
+      </div>
       <br> 
-      <label for="set2">Setting 2</label>      
-      <input type="checkbox" name="set2" id="">
+      <div class="option">
+        <label for="set2">Setting 2</label>      
+        <input type="checkbox" name="set2" id="">
+      </div>
     </div>
