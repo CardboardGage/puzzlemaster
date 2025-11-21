898be826c0b94ea2ce06190b879397e03fda84fc
amarohl <marohla100@gmail.com>
2025-11-21 12:29:15 -0600

    Cleaned up some commented out stuff when I was trying to get game width and height synced

--- JS/phaserTest.js
+++ JS/phaserTest.js
@@ -1,9 +1,5 @@
 var config = {
     type: Phaser.AUTO,
-    // creatContainer: true,
-    // behindCanvas: true,
-    // PointerEvent: "gameArea",
-    width: GAME_WIDTH,
     height: GAME_HIEGHT,
     scene: { preload, create, update }
 };
