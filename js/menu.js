debb4695da5ed03c7c544db68475a754ea8e5c6b
Gage Winsand <gwinsand1@student.cvtc.edu>
2025-11-25 17:07:13 -0600

    restored menu.js

---
+++ js/menu.js
@@ -0,0 +1,57 @@
+// button initialization
+$startBtn = $("#startBtn");
+$loginBtn = $("#loginBtn");
+$configBtn = $("#configBtn");
+$maintBtn = $("#maintBtn");
+$backBtn = $("#backBtn");
+$registerBtn = $("#registerBtn");
+$leaderboard = $("#leaderboard");
+$logoutBtn = $("#logoutBtn");
+
+$startBtn.on("click", ()=>{
+  console.log("Start Pressed");
+  $(".mainMenu").hide();
+  $leaderboard.hide();
+  $(".startMenu").show();
+  $backBtn.show();
+  window.location.href = "../screens/gameScreen.php";
+});
+
+$loginBtn.on("click", ()=>{
+  console.log("Login Pressed");
+  $(".mainMenu").hide();
+  $leaderboard.hide();
+  $(".loginMenu").show();
+  $(".startMenu").hide();
+  // $backBtn.show();
+  window.location.href = "../login.php";
+});
+
+$configBtn.on("click", ()=>{
+  console.log("Config Pressed");
+  $(".mainMenu").hide();
+  $leaderboard.hide();
+  $(".configMenu").show();
+  $(".startMenu").hide();
+  $backBtn.show();
+});
+
+$maintBtn.on("click", ()=>{
+  console.log("Maint Pressed");
+  $(".mainMenu").hide();
+  $leaderboard.hide();
+  window.location.href = "../screens/maintenanceMenu.php";
+});
+
+$backBtn.on("click", ()=>{
+  $(".mainMenu").show();
+  $leaderboard.show();
+  $(".configMenu").hide();
+  $(".loginMenu").hide();
+  $(".startMenu").hide();
+  $backBtn.hide();
+});
+
+$logoutBtn.on("click", () => {
+  window.location.href = "../logout.php";
+});
