592e57250108620d0084a619932daccb7fb40c17
mgadke3 <mgadke3@student.cvtc.edu>
2025-12-02 21:24:45 -0600

    Shifted labels for organization

--- screens/gameScreen.php
+++ screens/gameScreen.php
@@ -7,20 +7,18 @@ if (!isset($_SESSION["seed"])) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>gameScreen</title>
     <link rel="stylesheet" href="../css/gameScreen.css">
 </head>
-
 <body>
     <div class="wrapper">
         <header>
             <!-- menu button to pull up options/settings
             menu -->
-            <input type="button" value="menu" id="menuButton">
+            <input type="button" value="<- Menu" id="menuButton">
             <!-- just using placeholders right now 
             for whatever we hook these outputs up to -->
             <div class="level">
@@ -28,23 +26,21 @@ if (!isset($_SESSION["seed"])) {
                 <output name="Level: " for="level">01</output>
             </div>
             <div class="moves">
-                <p>Moves</p>
+                <p>Moves Left</p>
                 <output name="Moves: " for="moves">00</output>
             </div>
             <div class="total">
-                <p>Total</p>
+                <p>Total Points</p>
                 <output name="Total: " for="total">0000</output>
             </div>
-
+            <div class="score">
+                <p>Round Points</p>
+                <output name="Score: " for="score">000</output>
+            </div>
             <div class="target">
                 <p>Target</p>
                 <output name="Target: " for="target">00</output>
             </div>
-            <div class="score">
-                <p>Score</p>
-                <output name="Score: " for="score">0000</output>
-            </div>
-
         </header>
         <main>
             <aside>
@@ -73,10 +69,8 @@ if (!isset($_SESSION["seed"])) {
                 <script src="../js/matchLogic.js"></script>
                 <script src="../js/phaserTest.js"></script>
             </div>
-
         </main>
     </div>
     <aside hidden id="seed"><?= $_SESSION['seed'] ?></aside>
 </body>
-
 </html>
