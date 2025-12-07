0a05486a7512937325e028294e19788012b0f5d6
Gage Winsand <gwinsand1@student.cvtc.edu>
2025-12-06 18:09:01 -0600

    added styling links to deleteEntry, editEntry, and editUser

--- screens/admin/editUser.php
+++ screens/admin/editUser.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Edit User</title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-
+  <link rel="stylesheet" href="../../css/mainMenu.css">
   
 <?php 
 ini_set('display_errors', '1');
