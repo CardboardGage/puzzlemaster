e73b13737bb5b2645ed97e1c7f05fc77c5cad662
Matt <jgraber4@student.cvtc.edu>
2025-12-07 17:18:31 -0600

    fixes title for gameScreen.php

--- screens/gameScreen.php
+++ screens/gameScreen.php
@@ -11,7 +11,7 @@ require "../sanitize.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>gameScreen</title>
+    <title>Puzzlemaster</title>
     <link rel="stylesheet" href="../css/gameScreen.css">
 </head>
 <body>
